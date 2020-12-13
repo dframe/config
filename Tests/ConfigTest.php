c0cea4c5507dded992ec198362c6fe1c638ea69c
Sławomir Kaleta <slaszka@gmail.com>
2020-12-13 13:05:40 +0100

    Resolved #182, Move Test into separate folders

---
+++ Tests/ConfigTest.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Dframe\Config\Tests;
+
+use Dframe\Config\Config;
+use org\bovigo\vfs\vfsStream;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Class ConfigTest
+ *
+ * @package Dframe\Tests
+ */
+class ConfigTest extends TestCase
+{
+    /**
+     * @var vfsStream
+     */
+    private $fileSystem;
+
+    /**
+     *
+     */
+    public function testLoad()
+    {
+        $configTest = Config::load('test', $this->fileSystem->url('') . '/Config/');
+        $this->assertEquals('yes', $configTest->get('create'));
+    }
+
+    /**
+     *
+     */
+    public function testLoadIfNotExist()
+    {
+        $configTest = Config::load('test', $this->fileSystem->url('') . '/Config/');
+        $this->assertEquals('default_value', $configTest->get('not_exist', 'default_value'));
+    }
+
+    /**
+     *
+     */
+    protected function setUp(): void
+    {
+        $directory = [
+            'Config' => [
+                'test.php' => "<?php return ['create' => 'yes'];",
+            ],
+        ];
+
+        $this->fileSystem = vfsStream::setup('root', 755, $directory);
+    }
+}
